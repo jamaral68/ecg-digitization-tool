4c7cd1f212b96aff774c36b7e002f8ef945c306a
MatheusMorata <matlucar09@hotmail.com>
2026-03-06 09:41:58 -0300

    feat: Módulo setup

--- edt.py
+++ edt.py
@@ -2,6 +2,7 @@ import sys
 import cv2 as cv
 import numpy as np
 import strategy
+import setup
 from scipy import ndimage
 from matplotlib import pyplot as plt
 from edt_utils import is_nan, py_blockproc, display_segments, detect_ref_pulse, print_line_dict,segment_to_df, remove_text
@@ -25,64 +26,12 @@ def ecg_to_csv(image_name, template_name, csv_name, config_dict):
     dilation = config_dict['dilation']
     perc_max_dist = config_dict['perc_max_dist']
 
-    # the names dependending on the layout
-    if layout[1]== 4 and layout[0]==3:
-        lt_leads = [ ['I', 'aVR','V1','V4'],
-                        ['II','aVL','V2','V5'],
-                        ['III','aVF', 'V3','V6'],
-                        ['II']
-
-                        ]
-    elif layout[1]==2:
-        raise NotImplementedError ('Not implemented' )
-    elif layout[1]==1:
-
-       # raise NotImplementedError ('Not implemented' )
-       lt_leads = [ ['I'],
-                    ['II'],
-                    ['III'],
-                    ['aVR'],
-                    ['aVL'],
-                    ['aVF'],
-                    ['V1'],
-                    ['V2'],
-                    ['V3'],
-                    ['V4'],
-                    ['V5'],
-                    ['V6']]
-
-    else:
-        raise ValueError('columns must be 4, 2 or 1')
-    
-    # Define pulse detection
-    if pulse == 0 :
-        print("INFO: No pulse to be detected")
-    elif pulse == -1:
-        print("INFO: pulse to be detected in all lines")
-    elif  isinstance(pulse, list): 
-        print("INFO: pulse on lines: {}.".format(pulse)) 
-        for p in pulse:
-            lt_leads[p].append('Pulse')
-    elif isinstance(pulse, int): 
-        print("INFO: pulse on line: {}.".format(pulse)) 
-        lt_leads[pulse].append('Pulse')
-
-    else:
-        raise ValueError('pulse should  be 0, an int or a list')
-    
-    # Define rhythm
-    if rhythm == 0:
-        print("INFO: No rhythm lead") 
-    else:
-        print("INFO: rhythm on line : {}.".format(rhythm)) 
-
-    #load the image 
-    image = cv.imread(image_name)
-
-    # sanity check
-    if image is None:
-        print('Cannot open image: ' + image_name)
-        sys.exit(0)
+    # Usar a função setup_ecg para configurar tudo
+    try:
+        lt_leads, image = setup.setup_ecg(layout, pulse, rhythm, image_name)
+    except Exception as e:
+        print(f"Erro na configuração do ECG: {e}")
+        sys.exit(1)
 
     if verbose > 1:
         plt.imshow(image)
---
+++ setup.py
@@ -0,0 +1,68 @@
+import cv2
+import sys
+
+def setup_ecg(layout, pulse, rhythm, image_name):
+    """
+    Configuração completa para processamento de ECG
+    """
+    
+    # the names dependending on the layout
+    if layout[1]== 4 and layout[0]==3:
+        lt_leads = [ ['I', 'aVR','V1','V4'],
+                        ['II','aVL','V2','V5'],
+                        ['III','aVF', 'V3','V6'],
+                        ['II']
+
+                        ]
+    elif layout[1]==2:
+        raise NotImplementedError ('Not implemented' )
+    elif layout[1]==1:
+
+       # raise NotImplementedError ('Not implemented' )
+       lt_leads = [ ['I'],
+                    ['II'],
+                    ['III'],
+                    ['aVR'],
+                    ['aVL'],
+                    ['aVF'],
+                    ['V1'],
+                    ['V2'],
+                    ['V3'],
+                    ['V4'],
+                    ['V5'],
+                    ['V6']]
+
+    else:
+        raise ValueError('columns must be 4, 2 or 1')
+    
+    # Define pulse detection
+    if pulse == 0 :
+        print("INFO: No pulse to be detected")
+    elif pulse == -1:
+        print("INFO: pulse to be detected in all lines")
+    elif  isinstance(pulse, list): 
+        print("INFO: pulse on lines: {}.".format(pulse)) 
+        for p in pulse:
+            lt_leads[p].append('Pulse')
+    elif isinstance(pulse, int): 
+        print("INFO: pulse on line: {}.".format(pulse)) 
+        lt_leads[pulse].append('Pulse')
+
+    else:
+        raise ValueError('pulse should  be 0, an int or a list')
+    
+    # Define rhythm
+    if rhythm == 0:
+        print("INFO: No rhythm lead") 
+    else:
+        print("INFO: rhythm on line : {}.".format(rhythm)) 
+
+    #load the image 
+    image = cv2.imread(image_name)
+
+    # sanity check
+    if image is None:
+        print('Cannot open image: ' + image_name)
+        sys.exit(0)
+    
+    return lt_leads, image
