243ee26721bc25e54d0c574c2d94f4dc311260b5
MatheusMorata <matlucar09@hotmail.com>
2026-03-06 09:47:41 -0300

    feat: Funções retornam image_gray

--- strategy.py
+++ strategy.py
@@ -3,7 +3,7 @@ from edt_utils import extract_image
 
 
 def color(image, lower, upper, thres_value):
-    img_hsv=cv.cvtColor(image, cv.COLOR_BGR2HSV)
+    img_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
     #Filter color to remove the grid
     #lower=(0,0,0) # black colssor
     #upper=(179,255,220) # dark gray
@@ -11,18 +11,20 @@ def color(image, lower, upper, thres_value):
     result = img_hsv.copy()
     result[mask!=255] = (255, 255, 255) # if it is not very dark set it to white
     #Convert to gray scale
-    image = cv.cvtColor(result, cv.COLOR_HSV2BGR )
-    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    image_bgr = cv.cvtColor(result, cv.COLOR_HSV2BGR)
+    image_gray = cv.cvtColor(image_bgr, cv.COLOR_BGR2GRAY)
     # To binary image
-    ret, th1 = cv.threshold(image_gray, thres_value, 255,cv.THRESH_BINARY)
-    return ret, th1
+    ret, th1 = cv.threshold(image_gray, thres_value, 255, cv.THRESH_BINARY)
+    return ret, th1, image_gray
+
 
 def filter(image, kSize2d, kSize1d, thres_value):
     image_gray = extract_image(image, kSize2d, kSize1d)
-    ret, th1 = cv.threshold(image_gray, thres_value, 255,cv.THRESH_BINARY) # transform to binary
-    return ret, th1    
+    ret, th1 = cv.threshold(image_gray, thres_value, 255, cv.THRESH_BINARY) # transform to binary
+    return ret, th1, image_gray
+
 
 def none(image, thres_value):
     image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    ret, th1 = cv.threshold(image_gray, thres_value, 255,cv.THRESH_BINARY) # transform to binary
-    return ret, th1
+    ret, th1 = cv.threshold(image_gray, thres_value, 255, cv.THRESH_BINARY) # transform to binary
+    return ret, th1, image_gray
