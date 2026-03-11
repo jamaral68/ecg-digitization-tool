1ba6fe0c6ee20b29c34353803495ab2821c50144
MatheusMorata <matlucar09@hotmail.com>
2026-03-11 16:44:46 -0300

    REFACTOR: Implemented class setup

--- src/setup.py
+++ src/setup.py
@@ -1,68 +1,94 @@
-import cv2
-import sys
+class Setup:
+    def __init__(
+        self,
+        image,
+        strategy,
+        thres_value,
+        dilation,
+        perc_space_leads,
+        layout,
+        perc_max_dist,
+        pulse,
+        rhythm,
+        verbose,
+        mmpsec,
+        mmpmv,
+        pulse_width_mm,
+        pulse_height_mm,
+        pulse_per_sec,
+        pulse_per_mv,
+        sample_frequency,
+        time_lead,
+        location,
+        num_sampling_points,
+        lower,
+        upper,
+        kSize2d,
+        kSize1d,
+        template,     
+        csv_name      
+    ):
+        self.image = image
+        self.strategy = strategy
+        self.thres_value = thres_value
+        self.dilation = dilation
+        self.perc_space_leads = perc_space_leads
+        self.layout = layout
+        self.perc_max_dist = perc_max_dist
+        self.pulse = pulse
+        self.rhythm = rhythm
+        self.pulse_width_mm = pulse_width_mm
+        self.pulse_height_mm = pulse_height_mm
+        self.pulse_per_sec = pulse_per_sec
+        self.pulse_per_mv = pulse_per_mv
+        self.sample_frequency = sample_frequency
+        self.time_lead = time_lead
+        self.location = location
+        self.num_sampling_points = num_sampling_points
+        self.verbose = verbose
+        self.lower = lower
+        self.upper = upper
+        self.kSize2d = kSize2d
+        self.kSize1d = kSize1d
+        self.mmpsec = mmpsec
+        self.mmpmv = mmpmv
+        self.template = template
+        self.csv_name = csv_name
+        self.hpulse = 0
+        self.wpulse = 0
 
-def setup_ecg(layout, pulse, rhythm, image_name):
-    """
-    Complete configuration for ECG processing
-    """
-    
-    # the names dependending on the layout
-    if layout[1]== 4 and layout[0]==3:
-        lt_leads = [ ['I', 'aVR','V1','V4'],
-                        ['II','aVL','V2','V5'],
-                        ['III','aVF', 'V3','V6'],
-                        ['II']
+        # Automatically set leads based on layout
+        self.lt_leads = self._set_leads()
 
-                        ]
-    elif layout[1]==2:
-        raise NotImplementedError ('Not implemented' )
-    elif layout[1]==1:
+    def _set_leads(self):
+        """
+        Define lead names based on the layout (rows x columns)
+        """
+        rows, cols = self.layout
 
-       # raise NotImplementedError ('Not implemented' )
-       lt_leads = [ ['I'],
-                    ['II'],
-                    ['III'],
-                    ['aVR'],
-                    ['aVL'],
-                    ['aVF'],
-                    ['V1'],
-                    ['V2'],
-                    ['V3'],
-                    ['V4'],
-                    ['V5'],
-                    ['V6']]
-
-    else:
-        raise ValueError('columns must be 4, 2 or 1')
-    
-    # Define pulse detection
-    if pulse == 0 :
-        print("INFO: No pulse to be detected")
-    elif pulse == -1:
-        print("INFO: pulse to be detected in all lines")
-    elif  isinstance(pulse, list): 
-        print("INFO: pulse on lines: {}.".format(pulse)) 
-        for p in pulse:
-            lt_leads[p].append('Pulse')
-    elif isinstance(pulse, int): 
-        print("INFO: pulse on line: {}.".format(pulse)) 
-        lt_leads[pulse].append('Pulse')
-
-    else:
-        raise ValueError('pulse should  be 0, an int or a list')
-    
-    # Define rhythm
-    if rhythm == 0:
-        print("INFO: No rhythm lead") 
-    else:
-        print("INFO: rhythm on line : {}.".format(rhythm)) 
-
-    #load the image 
-    image = cv2.imread(image_name)
-
-    # sanity check
-    if image is None:
-        print('Cannot open image: ' + image_name)
-        sys.exit(0)
-    
-    return lt_leads, image
+        if cols == 4 and rows == 3:
+            return [
+                ['I', 'aVR', 'V1', 'V4'],
+                ['II', 'aVL', 'V2', 'V5'],
+                ['III', 'aVF', 'V3', 'V6'],
+                ['II']
+            ]
+        elif cols == 2:
+            raise NotImplementedError('Layout with 2 columns not implemented')
+        elif cols == 1:
+            return [
+                ['I'],
+                ['II'],
+                ['III'],
+                ['aVR'],
+                ['aVL'],
+                ['aVF'],
+                ['V1'],
+                ['V2'],
+                ['V3'],
+                ['V4'],
+                ['V5'],
+                ['V6']
+            ]
+        else:
+            raise ValueError('Columns must be 4, 2, or 1')
