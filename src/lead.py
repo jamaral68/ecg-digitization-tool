f41bdd5d44a26f312bda31b56c65daf1630af739
MatheusMorata <matlucar09@hotmail.com>
2026-03-26 10:55:16 -0300

    doc: Alterações nos comentários

--- src/lead.py
+++ src/lead.py
@@ -1,7 +1,7 @@
 class Lead:
     def __init__(self, name, coords):
         """
-        name: str - nome da lead (ex: 'I', 'V1')
+        name: str - 'I'
         coords: list[int] - [x1, y1, x2, y2]
         """
         self.name = name
