6cb5e85b3d827b62b8ade139037557a9af20a365
MatheusMorata <matlucar09@hotmail.com>
2026-03-26 10:50:57 -0300

    feat: Implementado classe lead

---
+++ src/lead.py
@@ -0,0 +1,8 @@
+class Lead:
+    def __init__(self, name, coords):
+        """
+        name: str - nome da lead (ex: 'I', 'V1')
+        coords: list[int] - [x1, y1, x2, y2]
+        """
+        self.name = name
+        self.coords = coords
