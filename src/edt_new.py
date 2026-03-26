628dc28f4eae3b4a1d0768dbd777e4a661755582
MatheusMorata <matlucar09@hotmail.com>
2026-03-26 10:50:27 -0300

    add: Adicionado lead.py

--- src/edt_new.py
+++ src/edt_new.py
@@ -1,44 +1,27 @@
 from ultralytics import YOLO
 import cv2 as cv
-import matplotlib.pyplot as plt
-import numpy as np
+
+"""
+def train_model(model, data, epochs, imgsz, device, workers):
+    
+    model = YOLO(model)
+    model.train(data=data, epochs=epochs, imgsz=imgsz, device=device, workers=workers)
+
+    return model 
+
+"""
 
 def ecg_to_csv():
+    model = YOLO("best.pt")
+    results = model("../teste.png")
 
-    model = YOLO("yolov8n.pt")
+    img = cv.imread("../teste.png")
 
-    image_path = '../teste.png'
+    for i, box in enumerate(results[0].boxes):
+        x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
 
-    model.train(data="dataset.yaml", epochs=50, imgsz=480, device="cpu", workers=8)
-    results = model.predict(image_path)
+        crop = img[y1:y2, x1:x2]
 
-    # Filtrar bounding boxes com confiança >= 0.5
-    filtered_boxes = []
-    filtered_scores = []
-
-    for box, conf in zip(results[0].boxes.xyxy, results[0].boxes.conf):
-        if conf >= 0.5:
-            filtered_boxes.append(box.numpy())  # converte tensor → numpy
-            filtered_scores.append(float(conf))
-
-    filtered_boxes = np.array(filtered_boxes)
-
-    # Carregar imagem original
-    img = cv.imread(image_path)
-    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-
-    # Exibir cada lead recortada
-    plt.figure(figsize=(15, 6))
-    for i, box in enumerate(filtered_boxes):
-        x1, y1, x2, y2 = map(int, box)
-        lead_crop = img[y1:y2, x1:x2]
-
-        plt.subplot(1, len(filtered_boxes), i+1)
-        plt.imshow(lead_crop)
-        plt.axis('off')
-        plt.title(f'Lead {i+1}')
-
-    plt.tight_layout()
-    plt.show()
+        
 
 ecg_to_csv()
