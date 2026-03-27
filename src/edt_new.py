3ba671b8e0cddc9b65627efecbdda6646eb444c4
MatheusMorata <matlucar09@hotmail.com>
2026-03-27 16:41:32 -0300

    refactor: edt_new.py

--- src/edt_new.py
+++ src/edt_new.py
@@ -7,36 +7,66 @@ def train_model(model, data, epochs, imgsz, device, workers):
     return model 
 
 """
+
+import cv2 as cv
+import numpy as np
 from ultralytics import YOLO
-import cv2
-import matplotlib.pyplot as plt
+from edt_utils import segment_to_df
 
-def ecg_to_csv(setup):
-    # Carrega o modelo
+image = '../teste.png'
+pulse_width_mm = 5
+pulse_height_mm = 10
+mmpsec = 25
+mmpmv = 10
+pulse_per_sec = pulse_width_mm / mmpsec
+pulse_per_mv = pulse_height_mm / mmpmv
+sample_frequency = 500
+time_lead = 2.5
+num_sampling_points = int(time_lead * sample_frequency)
+layout = (3, 4)
+
+def ecg_to_csv(img_path=image):
     model = YOLO("best.pt")
-    
-    # Faz a detecção
-    results = model(setup.image)
-    
-    # Carrega a imagem original com OpenCV
-    img = cv2.imread(setup.image)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Converte BGR para RGB
-    
-    # Itera sobre as detecções
-    for result in results:
-        boxes = result.boxes.xyxy.cpu().numpy()  # [x1, y1, x2, y2]
-        for i, box in enumerate(boxes):
-            x1, y1, x2, y2 = map(int, box)
-            # Recorta a região da bounding box
-            crop = img[y1:y2, x1:x2]
-            
-            # Exibe o recorte
-            plt.figure()
-            plt.imshow(crop)
-            plt.title(f"Detecção {i+1}")
-            plt.axis('off')
-            plt.show()
+    results = model(img_path)
+    result = results[0]
 
+    line_list = []
+    img = cv.imread(img_path)
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-# Exemplo de uso
-ecg_to_csv()
+    for box in result.boxes:
+        x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+        cls_id = int(box.cls[0])
+        lead_name = model.names[cls_id]
+        if lead_name == 'pulse':
+            continue
+
+        crop = img_gray[y1:y2, x1+10:x2-10]
+        height, width = crop.shape
+
+        yseg = np.array([height - np.argmin(crop[:, col]) for col in range(width)])
+        baseline = np.min(yseg)
+
+        line_list.append({
+            'wpulse': width,
+            'hpulse': height,
+            'curves': [{
+                'xseg': np.arange(width),
+                'yseg': yseg,
+                'wseg': width,
+                'baseline': baseline,
+                'name': lead_name
+            }]
+        })
+
+    df = segment_to_df(line_list, pulse_per_sec, pulse_per_mv, num_sampling_points)
+
+    lead_order = ['I', 'aVR', 'V1', 'V4',
+                  'II', 'aVL', 'V2', 'V5',
+                  'III', 'aVF', 'V3', 'V6']
+
+    for lead in lead_order:
+        if lead not in df.columns:
+            df[lead] = np.zeros(num_sampling_points)
+
+    return df
