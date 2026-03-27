eede3f7ed55874fb0ca63a2c31bc9b0556e3edd2
MatheusMorata <matlucar09@hotmail.com>
2026-03-27 12:56:53 -0300

    add: Adicionado main_new.py

--- src/lead.py
+++
@@ -1,8 +0,0 @@
-class Lead:
-    def __init__(self, name, coords):
-        """
-        name: str - 'I'
-        coords: list[int] - [x1, y1, x2, y2]
-        """
-        self.name = name
-        self.coords = coords
