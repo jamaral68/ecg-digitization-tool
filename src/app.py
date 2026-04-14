712c911c8c2d0cb86b0e446bd5a256c6a999d272
MatheusMorata <matlucar09@hotmail.com>
2026-04-14 10:37:30 -0300

    feat: No multiple files

--- src/app.py
+++ src/app.py
@@ -2,10 +2,8 @@ import streamlit as st
 
 st.title("ECG Digitization Tool")
 
-arquivo = st.file_uploader("Carregar arquivo", type=["png", "jpg", "jpeg", "pdf", "csv"])
+arquivo = st.file_uploader("Carregar arquivo", type=["png", "jpg", "jpeg"], accept_multiple_files=False)
 
 if arquivo is not None:
     st.success(f"Arquivo carregado: {arquivo.name}")
-
-    if arquivo.type in ["image/png", "image/jpeg"]:
-        st.image(arquivo)
+    st.image(arquivo)
