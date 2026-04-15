19bd838924ad7d4d24d321df7b205635bdcd7c52
MatheusMorata <matlucar09@hotmail.com>
2026-04-15 13:41:40 -0300

    feat: print metrics

--- src/main.py
+++ src/main.py
@@ -52,9 +52,11 @@ if __name__ == "__main__":
         df.to_csv(setup.csv_name, index=False)
         print(f"INFO: Saved {csv_name}")
 
-
+    print("=======================================")
+    print("METRICS")
     print(f"mAP50-95: {metrics.box.map:.4f}")
     print(f"mAP50:    {metrics.box.map50:.4f}")
     print(f"Precision: {metrics.box.mp:.4f}")
     print(f"Recall:    {metrics.box.mr:.4f}")
+    print("=======================================")
     print("THE END")
